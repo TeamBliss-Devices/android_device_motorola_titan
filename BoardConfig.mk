fdf3dd7b634a3f9363620981e428782b467131d7
koradiavatsal <koradiavatsal@gmail.com>
2015-09-17 08:41:54 +0200

    Bliss-ify

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,3 +47,33 @@ TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
 # inherit from the proprietary version
 -include vendor/motorola/titan/BoardConfigVendor.mk
+
+# Block_Build
+Bliss_Build_Block := 1 
+
+# BlissPop Configs
+BLISS_WIPE_CACHES := 0
+BLISSIFY := true
+BLISS_O3 := true
+BLISS_GRAPHITE := true
+BLISS_STRICT := false
+BLISS_KRAIT := false
+BLISS_PIPE := true
+FLOOP_NEST_OPTIMIZE := true
+ENABLE_GCCONLY := true
+TARGET_USE_QCOM_BIONIC_OPTIMIZATION := true
+FAST_MATH := true
+ENABLE_MODULAR_O3 := true
+ENABLE_LTO := true
+Link_Time_Optimizations := true
+TARGET_USE_ION_COMPAT := true
+TARGET_USE_KRAIT_PLD_SET := false
+TARGET_TC_ROM := 4.8-linaro
+TARGET_TC_KERNEL := 4.8-linaro
+TARGET_GCC_VERSION_EXP := $(TARGET_TC_ROM)
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := $(TARGET_TC_KERNEL)
+BLISS_DEVELOPER := Vatsal
+
+
+#SaberMod
+-include vendor/bliss/config/sm.mk
